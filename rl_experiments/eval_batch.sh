13b46892f5b9fd59c166b81abb01c4fe140f4bb6
Angel Ayala <angel4ayala@gmail.com>
2024-11-19 18:07:57 +1100

    updating batch scripts

--- rl_experiments/eval_batch.sh
+++ rl_experiments/eval_batch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python eval.py --logspath './logs_drone_vector/ddqn-srl_2023-12-12_19-36-38'
+python eval.py --logspath './logs_drone_vector/ddqn-srl_2023-12-12_19-36-38' --load-config
+python eval_td3_cf.py --logspath 'logs_cf_vector/td3-srl_2024-11-19_17-33-24' --load-config
--- rl_experiments/learn_batch.sh
+++ rl_experiments/learn_batch.sh
@@ -43,3 +43,28 @@ python learn.py \
 --eval-steps 0 \
 --use-cuda \
 --seed 202401
+
+# TD3 vector-based (16,)
+python learn_td3_cf.py \
+--frame-stack 3 \
+--zone-steps 10 \
+--is-vector \                        
+--add-target-pos \
+--uav-data=imu,gyro,gps,gps_vel,north \
+--memory-steps 128 \
+--eval-steps 0 \
+--use-cuda \
+--seed 202401
+
+# TD3-SRL-Dist vector-based (16,) can be used with SAC also
+python learn_td3_cf.py \
+--frame-stack 3 \
+--zone-steps 10 \
+--is-vector \                        
+--add-target-pos \
+--uav-data=imu,gyro,gps,gps_vel,north \
+--is-srl --model-vector-target-dist \
+--eval-steps 0 \
+--use-cuda \
+--seed 202401
+
